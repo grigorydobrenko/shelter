3bd305dd1b9a0b617ddaa9714ba423ffc19de3e3
Grigory Dobrenko <grigorydobrenko97@gmail.com>
2022-04-24 01:05:32 +0300

    feat: add burger main

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,7 +1,14 @@
+// Menu burger
+
+
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
 const body = document.querySelector('body');
 const logo = document.querySelector('.header__logo');
+
+const overlay = document.querySelector('.overlay');
+
+
 
 function toggleMenu() {
   menu.classList.toggle('menu_active');
@@ -16,12 +23,29 @@ function toggleHidden() {
   body.classList.toggle('hidden');
 }
 
+function overLaid() {  
+  overlay.classList.toggle('display-block') 
+}
 
-
-
+overlay.addEventListener('click',overLaid);
+overlay.addEventListener('click',toggleMenu);
+overlay.addEventListener('click',rotateBurger);
+overlay.addEventListener('click',toggleHidden);
 
 burger.addEventListener('click', () => {
   toggleMenu();
   rotateBurger();
   toggleHidden();
+  overLaid();
+})
+
+
+menu.addEventListener('click', (event) => {
+  console.log(event);
+  if (event.target.nodeName == 'A') {
+    toggleMenu();
+    overLaid();
+    rotateBurger();
+    toggleHidden();
+  }
 })
