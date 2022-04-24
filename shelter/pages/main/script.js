ef5c7bae653a495733a68cac1e564c0d6971a3dd
Grigory Dobrenko <grigorydobrenko97@gmail.com>
2022-04-24 16:06:02 +0300

    feat: finished burger main

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,51 +1,57 @@
 // Menu burger
 
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+const body = document.querySelector("body");
+const logo = document.querySelector(".header__logo");
 
-const burger = document.querySelector('.burger');
-const menu = document.querySelector('.menu');
-const body = document.querySelector('body');
-const logo = document.querySelector('.header__logo');
-
-const overlay = document.querySelector('.overlay');
-
-
+const overlay = document.querySelector(".overlay");
+const header = document.querySelector(".header__container");
 
 function toggleMenu() {
-  menu.classList.toggle('menu_active');
-  logo.classList.toggle('header__logo_active');
+  menu.classList.toggle("menu_active");
+  logo.classList.toggle("header__logo_active");
 }
 
 function rotateBurger() {
-  burger.classList.toggle('burger_active');
+  burger.classList.toggle("burger_active");
 }
 
 function toggleHidden() {
-  body.classList.toggle('hidden');
+  body.classList.toggle("hidden");
 }
 
-function overLaid() {  
-  overlay.classList.toggle('display-block') 
+function overLaid() {
+  overlay.classList.toggle("display-block");
 }
 
-overlay.addEventListener('click',overLaid);
-overlay.addEventListener('click',toggleMenu);
-overlay.addEventListener('click',rotateBurger);
-overlay.addEventListener('click',toggleHidden);
+overlay.addEventListener("click", overLaid);
+overlay.addEventListener("click", toggleMenu);
+overlay.addEventListener("click", rotateBurger);
+overlay.addEventListener("click", toggleHidden);
 
-burger.addEventListener('click', () => {
+burger.addEventListener("click", () => {
   toggleMenu();
   rotateBurger();
   toggleHidden();
   overLaid();
-})
+});
 
-
-menu.addEventListener('click', (event) => {
+menu.addEventListener("click", (event) => {
   console.log(event);
-  if (event.target.nodeName == 'A') {
+  if (event.target.nodeName == "A") {
     toggleMenu();
     overLaid();
     rotateBurger();
     toggleHidden();
   }
-})
+});
+
+logo.addEventListener("click", (event) => {
+  if (menu.classList.contains("menu_active")) {
+    toggleMenu();
+    overLaid();
+    rotateBurger();
+    toggleHidden();
+  }
+});
